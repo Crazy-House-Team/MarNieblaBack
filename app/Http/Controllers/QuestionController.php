203277a4cea3a11325f0637aedc232743632906c
danimartinjuarez <danimartinjuarez@gmail.com>
2022-09-16 14:21:06 +0200

    Api read

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 
 class QuestionController extends Controller
 {
-    //
+    public function index()
+    {
+        $events = Question::all();
+        return response()->json($events, 200);
+    }
+
+    
 }
+
+
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\QuestionController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/v1/', [QuestionController::class, 'index'])->name('QuestionsApi');
+//Route::delete('/deleteEvent/{id}', [EventController::class, 'destroy'])->name('destroyEventApi');
+
